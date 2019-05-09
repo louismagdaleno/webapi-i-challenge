1a89a84b28a9da2de738ee5477270ee622a5cb03
Louis Magdaleno <buildthatapp@gmail.com>
2019-05-08 22:14:50 -0700

    Added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // implement your API here
 const express = require('express');
 const db = require('./data/db');
+const cors = require("cors");
 const server = express();
 
 server.get('/', (req, res) => {
