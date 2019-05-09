b1a373466bf2883719b4dba900ca3d56774b2b04
Louis Magdaleno <buildthatapp@gmail.com>
2019-05-08 21:59:22 -0700

    Added express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello from Express');
+});
+
+server.listen(5000, () => 
+console.log('Server running on http://localhost:5000')
+);
