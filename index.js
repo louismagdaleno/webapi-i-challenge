e51048f087818441e3a67bb13e44d6d9e6aec1aa
Louis Magdaleno <buildthatapp@gmail.com>
2019-05-08 22:12:30 -0700

    Added GET for /api/users/:id

--- index.js
+++ index.js
@@ -21,3 +21,18 @@ server.get('/api/users', (req, res) => {
             res.status(500).json({message: "server error"});
         });
 });
+
+server.get('/api/users/:id', (req, res) => {
+	const { id } = req.params;
+
+	db
+		.findById(id)
+		.then((user) => {
+			if (user) {
+				res.status(200).json(user);
+			} else {
+				res.status(404).json({ message: `user not found` });
+			}
+		})
+		.catch((err) => res.status(500).json({ message: `Can't get user data!!` }));
+});
