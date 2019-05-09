adb1e62636fe6cee0a1cace41bc71d4579b14f8c
Louis Magdaleno <buildthatapp@gmail.com>
2019-05-08 22:11:47 -0700

    Added GET for /api/users

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ server.get('/', (req, res) => {
 server.listen(5000, () => 
 console.log('Server running on http://localhost:5000')
 );
+
+server.get('/api/users', (req, res) => {
+    db
+        .find()
+        .then((users) => {
+            res.status(200).json(users);
+        })
+        .catch((err) => {
+            res.status(500).json({message: "server error"});
+        });
+});
