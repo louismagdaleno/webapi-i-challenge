6da9083f9b1c10009b54dd36c39b641964e74c88
Louis Magdaleno <buildthatapp@gmail.com>
2019-05-17 00:46:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,99 +1,76 @@
 // implement your API here
 const express = require('express');
-const db = require('./data/db');
-const cors = require("cors");
+
 const server = express();
 
-server.get('/', (req, res) => {
-    res.send('Hello from Express');
-});
+const db = require('./data/db.js');
 
-server.listen(8080, () => 
-console.log('Server running on http://localhost:8080')
-);
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('<h2>Hello World</h2>');
+});
 
 server.get('/api/users', (req, res) => {
-    db
-        .find()
-        .then((users) => {
-            res.status(200).json(users);
-        })
-        .catch((err) => {
-            res.status(500).json({message: "server error"});
-        });
+    db.find()
+    .then(users => {
+        res.status(201).json(users);
+    })
+    .catch(({error, message}) => {
+        res.status(500).json({error : message});
+    });
 });
 
-server.post("/api/users/", (req, res) => {
-    if (!req.body.name || !req.body.bio) {
-      res
-        .status(400)
-        .json({ message: "Please provide name and bio for the user." });
-    } else
-      db.insert(req.body)
-        .then(newUser => res.status(201).json(newUser))
-        .catch(err =>
-          res
-            .status(500)
-            .json({
-              message: "There was an error while saving the user to the database."
-            })
-        );
-  });
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+
+    db.insert(user)
+    .then(user => {
+        res.status(201).json(user);
+    })
+    .catch(({error, message}) => {
+        res.status(500).json({error: message});
+    
+    })
+});
 
 server.get('/api/users/:id', (req, res) => {
-	const { id } = req.params;
+    const { id } = req.params;
 
-	db
-		.findById(id)
-		.then((user) => {
-			if (user) {
-				res.status(200).json(user);
-			} else {
-				res.status(404).json({ message: `user not found` });
-			}
-		})
-		.catch((err) => res.status(500).json({ message: `Can't get user data!!` }));
+    db.findById(id)
+    .then(user => {
+        res.status(201).json(user);
+    })
+    .catch(({ err, message }) => {
+        res.status(500).json({err : message})
+    });
 });
 
-server.delete("/api/users/:id", (req, res) => {
+server.delete('/api/users/:id', (req, res) => {
     const { id } = req.params;
-  
-    db.remove(id)
-      .then(user => {
-        if (!user)
-          res
-            .status(404)
-            .json({ message: "The user with the specified ID does not exists." });
-        else res.json(user);
-      })
-      .catch(err =>
-        res.status(500).json({ message: "The user could not be removed." })
-      );
-  });
 
-  server.put("/api/users/:id", (req, res) => {
+    db.remove(id)
+    .then(removed => {
+        res.status(201).json(removed);
+    })
+    .catch(({error, message}) => {
+        res.status(500).json({error: message});
+    });
+});
+
+server.put('/api/users/:id', (req, res) => {
     const { id } = req.params;
-  
-    if (!req.body.name && !req.body.bio) {
-      console.log("Wrong info");
-      res
-        .status(400)
-        .json({ errorMessage: "Please provide a name or bio for the user." });
-    } else
-      db.update(id, req.body)
-        .then(user => {
-          console.log("In request");
-          if (!user)
-            res
-              .status(404)
-              .json({
-                message: "The user with the specified ID does not exists. "
-              });
-          res.json(user);
-        })
-        .catch(err =>
-          res
-            .status(500)
-            .json({ error: "The user information could not be modified." })
-        );
-  });
+    const body = req.body;
+
+    db.update(id, body)
+    .then( updated => {
+        res.status(201).json(updated);
+    })
+    .catch(({error, message}) => {
+        res.status(500).json({error : message});
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000')
+});
